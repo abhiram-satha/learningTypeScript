19dcd1a515d5a9307d960886f35bbc27282d0ddd
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-22 20:37:23 +0000

    Removing errors from TypeScript file

--- using-ts.ts
+++ using-ts.ts
@@ -1,11 +1,11 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+const button2 = document.querySelector("button");
+const input12 = document.getElementById("num1")! as HTMLInputElement;
+const input22 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
+function addNum(num1: number, num2: number) {
   return num1 + num2;
 }
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
+button2.addEventListener("click", function() {
+  console.log(addNum(+input12.value, +input22.value));
 });
