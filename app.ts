335804943b9af280807cbc6a4ce95acdce74c84b
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-24 20:54:51 +0000

    Adding tuble to array

--- app.ts
+++ app.ts
@@ -1,4 +1,13 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  details: {
+    height: string;
+    weight: string;
+  }
+  role: [number, string]
+} = {
   name: "Abhiram",
   age: 28,
   hobbies: ["Sports", "Cooking"],
@@ -6,13 +15,14 @@ const person = {
     height: "6 feet",
     weight: "16 lbs",
   },
+  role: [2, "student"],
 };
 
 let favouriteActivites: string[];
 favouriteActivites = ["Sports"];
 console.log(person);
 
-for(const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase())
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
   // console.log(hobby.map()) ===> this would result in an error because TS knows hobby is a string not an array
 }
