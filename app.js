7ab8ad164d5ade5f1bd4c51c56e15bd4c1eaf43e
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-24 17:53:02 +0000

    Adding boolean type

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error("Incorrect input!");
+function add(n1, n2, showResult) {
+    // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error("Incorrect input!")
+    // }
+    if (showResult) {
+        console.log(n1 + n2);
     }
     return n1 + n2;
 }
-var number1 = "5";
+var number1 = 5.0;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var result = add(number1, number2, printResult);
 console.log(result);
