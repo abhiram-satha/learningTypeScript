86e356508b71d642397acd35e5e5c59471468031
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-25 23:43:35 +0000

    Adding enum to combine function

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+var Role2;
+(function (Role2) {
+    Role2[Role2["asnumber"] = 0] = "asnumber";
+    Role2[Role2["astext"] = 1] = "astext";
+})(Role2 || (Role2 = {}));
+;
 function combine(input1, input2, resultType) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number" || resultType === 'as-number') {
+    if (typeof input1 === "number" && typeof input2 === "number" || resultType === 0) {
         result = +input1 + +input2;
     }
     else {
@@ -13,9 +19,9 @@ function combine(input1, input2, resultType) {
     // }
     return result;
 }
-var combinedAges = combine(30, 26, "as-number");
+var combinedAges = combine(30, 26, 0);
 console.log(combinedAges);
-var combinedStringAges = combine("30", "26", "as-number");
+var combinedStringAges = combine("30", "26", 0);
 console.log(combinedStringAges);
-var combinedNames = combine("Max", "Anna", "as-text");
+var combinedNames = combine("Max", "Anna", 1);
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
+enum Role2 {'asnumber', 'astext'};
+
 function combine(
   input1: number | string,
   input2: number | string,
-  resultType: string
+  resultType: Role2.asnumber |  Role2.astext
 ) {
   let result;
 
-  if (typeof input1 === "number" && typeof input2 === "number" || resultType === 'as-number') {
+  if (typeof input1 === "number" && typeof input2 === "number" || resultType === 0) {
     result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
@@ -18,13 +20,13 @@ function combine(
   return result;
 }
 
-const combinedAges = combine(30, 26, "as-number");
+const combinedAges = combine(30, 26, 0);
 
 console.log(combinedAges);
 
-const combinedStringAges = combine("30", "26", "as-number");
+const combinedStringAges = combine("30", "26", 0);
 console.log(combinedStringAges);
 
-const combinedNames = combine("Max", "Anna", "as-text");
+const combinedNames = combine("Max", "Anna", 1);
 
 console.log(combinedNames);
