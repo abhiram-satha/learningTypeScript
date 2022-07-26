97b2c499e86f50146cb719081421472c00f7c5a9
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-26 00:09:08 +0000

    Adding undefined to function type

--- app.ts
+++ app.ts
@@ -8,4 +8,12 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function printNum(num: number): undefined {
+  //undefined because there is no value actually being returned could also use void
+  console.log("Result: " + num);
+  return;
+}
+
 printResult(add(5, 2));
+
+// let someValue: undefined;
