546d4551ad69d807ecb345d94033e27dd4e42529
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-29 22:27:40 +0000

    Adding default to add function

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,10 @@
 "use strict";
 const userName = "Abhiram";
+//Arrow function  => return not needed if expression can be entered on one line
+const add = (a, b) => a + b;
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => console.log("click"));
+}
+console.log(add(8, 7));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@
 
 
 //Arrow function  => return not needed if expression can be entered on one line
- const add = (a: number, b: number) => a + b;
+ const add = (a: number, b: number = 1) => a + b;
 
 const button = document.querySelector('button')
 
 if (button) {
 
-  button.addEventListener('click', event => console.log(event))
+  button.addEventListener('click', event => console.log("click"))
 }
 
  console.log(add(8,7))
