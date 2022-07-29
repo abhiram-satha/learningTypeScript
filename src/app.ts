e69f481725bcac32257e29a89aeaf177a7817629
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-29 22:18:04 +0000

    Adding button to app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,13 @@
 //Arrow function  => return not needed if expression can be entered on one line
  const add = (a: number, b: number) => a + b;
 
+const button = document.querySelector('button')
+
+if (button) {
+
+  button.addEventListener('click', event => console.log(event))
+}
+
  console.log(add(8,7))
 
 
