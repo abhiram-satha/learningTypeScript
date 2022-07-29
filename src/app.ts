20066404b80fc5972482966aee122af031dcea85
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-29 00:13:48 +0000

    Creating arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
  const userName = "Abhiram"
 
 
+//Arrow function  => return not needed if expression can be entered on one line
+ const add = (a: number, b: number) => a + b;
 
- 
+ console.log(add(8,7))
+
+
