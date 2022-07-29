17a8a2457714f190d98bbbed894a7920ccb2cdcf
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-29 22:59:47 +0000

    Using rest parameters

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,21 @@
 "use strict";
 const userName = "Abhiram";
 //Arrow function  => return not needed if expression can be entered on one line
-const add = (a, b) => a + b;
+const add = (a, b = 1) => a + b;
 const button = document.querySelector('button');
 if (button) {
     button.addEventListener('click', event => console.log("click"));
 }
 console.log(add(8, 7));
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['hiking', ...hobbies];
+const person = {
+    name: "Abhiram",
+    age: 30
+};
+const copiedPerson = Object.assign({}, person);
+const people = person;
+people.name = "Max";
+console.log(person.name);
+console.log(copiedPerson.name);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,39 @@
- const userName = "Abhiram"
+//  const userName = "Abhiram"
 
 
-//Arrow function  => return not needed if expression can be entered on one line
- const add = (a: number, b: number = 1) => a + b;
+// //Arrow function  => return not needed if expression can be entered on one line
+//  const add = (a: number, b: number = 1) => a + b;
 
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-if (button) {
+// if (button) {
 
-  button.addEventListener('click', event => console.log("click"))
-}
+//   button.addEventListener('click', event => console.log("click"))
+// }
 
- console.log(add(8,7))
+//  console.log(add(8,7))
 
  const hobbies = ['Sports', 'Cooking'];
 
  const activeHobbies = ['hiking', ...hobbies];
 
+ const person = {
+   name: "Abhiram",
+   age: 30
+ }
 
 
+ const copiedPerson = {...person};
 
+ console.log(copiedPerson.name)
+
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((currResult, currValue)=> {
+    return currResult + currValue
+  }, 0)
+}
+
+const addNumbers = add(5, 10, 2, 3.7)
 
 
