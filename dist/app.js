9d338d335e46c0a9e1fdeffce5f071cbfc80f718
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-08-02 01:18:58 +0000

    Adding a new class Department

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,9 @@
 "use strict";
-//  const userName = "Abhiram"
-// //Arrow function  => return not needed if expression can be entered on one line
-//  const add = (a: number, b: number = 1) => a + b;
-// const button = document.querySelector('button')
-// if (button) {
-//   button.addEventListener('click', event => console.log("click"))
-// }
-//  console.log(add(8,7))
-const hobbies = ['Sports', 'Cooking'];
-//destructuring arrays
-const [hobby1, hobby2, ...remaininghobbies] = hobbies;
-const activeHobbies = ['hiking', ...hobbies];
-const person = {
-    firstName: "Abhiram",
-    age: 30
-};
-//Object Destructuring
-const { firstName, age } = person;
-const copiedPerson = Object.assign({}, person);
-console.log(copiedPerson.firstName);
-const add = (...numbers) => {
-    return numbers.reduce((currResult, currValue) => {
-        return currResult + currValue;
-    }, 0);
-};
-const addNumbers = add(5, 10, 2, 3.7);
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Department("Accounting");
+console.log(accounting);
 //# sourceMappingURL=app.js.map
