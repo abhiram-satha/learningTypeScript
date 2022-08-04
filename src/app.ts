2819ec4492c3165de631fc1e7942960b9c981f30
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-08-04 00:28:50 +0000

    Adding interface for function

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,40 @@
-interface Greetable {
-  name: string;
+interface Named {
+  readonly name: string;
+}
+
+interface Greetable extends Named {
   age: number;
 
   greet(phrase: string): void;
 }
 
-class Person implements Greetable{
+class Person implements Greetable {
   name: string;
-  age: number
-  constructor(n:string, age:number) {
-    this.name = n
+  age: number;
+  constructor(n: string, age: number) {
+    this.name = n;
     this.age = age;
   }
   greet(phrase: string) {
-    console.log(`${phrase} ${this.name}`)
+    console.log(`${phrase} ${this.name}`);
   }
-
 }
 
 let user1: Greetable;
 
-user1 = new Person('Abhiram', 28)
+user1 = new Person("Abhiram", 28);
 
-user1.greet("Hello")
+user1.greet("Hello");
 
 console.log(user1);
+
+
+interface AddFn {
+  (a: number, b: number): number
+}
+
+let add: AddFn;
+
+add = (a: number, b: number) => {
+  return a + b
+}
