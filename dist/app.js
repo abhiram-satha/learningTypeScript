1150f33a9d9d2248d801dca4fc8765cfd080139e
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-08-04 00:18:56 +0000

    Using interdace to create class

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,15 @@
 "use strict";
-let user1;
-user1 = {
-    name: "Abhiram",
-    age: 28,
+class Person {
+    constructor(n, age) {
+        this.name = n;
+        this.age = age;
+    }
     greet(phrase) {
         console.log(`${phrase} ${this.name}`);
     }
-};
+}
+let user1;
+user1 = new Person('Abhiram', 28);
 user1.greet("Hello");
+console.log(user1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
-interface Person {
+interface Greetable {
   name: string;
   age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
-
-user1 = {
-  name: "Abhiram",
-  age: 28,
-
+class Person implements Greetable{
+  name: string;
+  age: number
+  constructor(n:string, age:number) {
+    this.name = n
+    this.age = age;
+  }
   greet(phrase: string) {
     console.log(`${phrase} ${this.name}`)
   }
+
 }
 
+let user1: Greetable;
+
+user1 = new Person('Abhiram', 28)
+
 user1.greet("Hello")
+
+console.log(user1);
